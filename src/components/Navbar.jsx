04f42ea007a3a2ae157506a65d72d82575ece872
Alessandro Parrilla <paggio96@gmail.com>
2022-09-05 10:21:28 +0200

    Installato Tailwind CSS e creato i 3 components Navbar, Routes, Footer

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,9 @@
+const Navbar = () => {
+  return (
+    <div className="p-5 pb-0 flex flex-wrap sm:justify-center items-center border-b dark:border-gray-700 border-gray-200 ">
+      <h1 className="text-8xl">Navbar</h1>
+    </div>
+  )
+}
+
+export default Navbar
